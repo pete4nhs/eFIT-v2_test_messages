bd303e6befb331d6f4cd43a811ad413476ce59bc
pete4nhs <165719339+pete4nhs@users.noreply.github.com>
2026-04-17 19:38:26 +0100

    Fix video file path in Tutorial page

--- pages/2_Tutorial.py
+++ pages/2_Tutorial.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.title('**Tutorial**')
 st.write('Please have a look at our video for a short tutorial on how to use the eFIT tool')
 
-video_file = open('pages/video_tutorial.mp4', 'rb')
+video_file = open('pages/video_tutorial.mp4': url: "https://raw.githubusercontent.com/pete4nhs/eFIT-v2.0/main/pages/video_tutorial.mp4"}, 'rb')
 video_bytes = video_file.read()
 
 st.video(video_bytes)
