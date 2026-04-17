6e8e5f211330500dd17d1009cf4ebdd2a4298fe3
pete4nhs <165719339+pete4nhs@users.noreply.github.com>
2026-04-17 20:57:36 +0100

    Add files via upload

---
+++ pages/2_Tutorial_test.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# This is to add in a title for our web app's page
+st.title('**Tutorial**')
+st.write('Please have a look at our video for a short tutorial on how to use the eFIT tool')
+
+video_url = "https://raw.githubusercontent.com/pete4nhs/eFIT-v2.0/main/pages/video_tutorial.mp4"
+st.video(video_url)
+
+st.write('')
+st.write('')
+st.write('')
+st.write(' **To report any bug, feedback or special requests please email: peter.saiu@nhs.net** ')
+st.write('')
+st.success('Please let us know if your organisation has used this tool')
